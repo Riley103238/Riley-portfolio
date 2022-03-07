3b89d9e594150ca4ada81e565863db91d0738ceb
Riley103238 <100111653+Riley103238@users.noreply.github.com>
2022-03-06 18:11:08 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,10 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "It's All Up To You To Decide";
 }
 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
